6fddc5bd6c1d13d33abed6b4a6086f7292249349
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-18 03:54:21 +0530

    practise form vali

---
+++ form_valid/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ form_valid/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,96 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css'
+})
+export class ReactiveFormComponent {
+
+  Signup_form = new FormGroup({
+    // username : new FormControl("", Validators.required),
+    // pattern
+    username : new FormControl("", [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+
+    email : new FormControl("", [Validators.required, Validators.email]),
+    password : new FormControl("", [Validators.required, Validators.minLength(6)]),
+
+    confirmPassword : new FormControl("", Validators.required)
+});
+
+formSubmitted : boolean = false 
+
+
+handle_form() {
+  // console.log(this.Signup_form);
+  this.formSubmitted = true
+
+  if(!this.pass_match()){
+   alert("Password does not matched")
+     
+  }else{
+    alert("signup successfull")
+
+
+    let user = {
+      username : this.Signup_form.value.username,
+      email : this.Signup_form.value.email,
+      password : this.Signup_form.value.password,
+      confirmPassword : this.Signup_form.value.confirmPassword
+  
+    }
+  
+    console.log("user", user)
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// getter for username
+ get uservaldtr () {
+   return this.Signup_form.get('username')
+ }
+
+//  emailvalid    AbstractControl | null 
+ get emailvalidr () {
+   return this.Signup_form.get('email')
+ }
+
+
+//  password
+get pasvald () {
+   return this.Signup_form.get("password")
+}
+
+
+// confirm_password  
+
+
+
+get confirmvalid() {
+    return this.Signup_form.get("confirmPassword")
+}
+
+
+
+//  password match
+pass_match() : boolean {
+  return this.Signup_form.value.password == this.Signup_form.value.confirmPassword;
+}
+
+
+
+}
